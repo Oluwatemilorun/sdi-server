3080c9ee3a9b7e4ccf6618c880b4f327bd16bb43
Isaac Oluwatemilorun <akiolisa@gmail.com>
2019-08-28 20:24:47 +0100

    more extension allowed

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 import os
 
+# TODO: set all this in the enviroment variables
 basedir = os.path.abspath(os.path.dirname(__file__))
+DATABASE_USER = 'akiolisa'
+DATABASE_PASSWORD = 'aki12345'
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = "postgresql://akiolisa:aki12345@localhost/rsg_sdi"
 ENGINE_DATABASE_URI = "postgresql://akiolisa@localhost" # TODO get the current localhost connection
 SECRET_KEY = "\/xc3*%\/x8f\/x11\/xd5\/xab\/xd6%\/x91\/x15\/xa2\/xa2\/x1foh\/x98c\/xc8\/x88B\/xd1!\/x95"
-UPLOAD_FOLDER = "/uploads"
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'shp', 'jpg', 'png', 'jpeg', 'tif', 'tiff', 'bmp', 'csv'}
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = ['txt', 'pdf', 'shp', 'shx', 'dbf', 'lyr', 'prj', 'prg', 
+ 'jpg', 'png', 'jpeg', 'tif', 'tiff', 'bmp', 'csv', 'zip']
